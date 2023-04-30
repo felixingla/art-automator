86ef3c37c7a1a1f080bc0ecd60e1d19904d622a1
fingla <felixinglavf@gmail.com>
2023-04-30 18:26:05 +0200

    Add signature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 
 function RandomCircles() {
-  const [numCircles, setNumCircles] = useState(getRandomNumber(1, 50));
+  const [numCircles, setNumCircles] = useState(getRandomNumber(25, 30));
   const canvasRef = useRef(null);
 
   useEffect(() => {
@@ -22,6 +22,8 @@ function RandomCircles() {
     canvas.style.width = `${window.innerWidth}px`;
     canvas.style.height = `${window.innerHeight}px`;
 
+    const colors = ["#DA615C", "#4998C9", "#F0C165"];
+
     const circles = [];
     let i = 0;
     let previousRadius = 10;
@@ -29,16 +31,15 @@ function RandomCircles() {
       const radius = previousRadius * 1.10;
       const x = Math.random() * (canvas.width - radius * 2) + radius;
       const y = Math.random() * (canvas.height - radius * 2) + radius;
-      const color = ["#DA615C"][Math.floor(Math.random() * 1)];
-      const number = i + 1;
-      circles.push({ x, y, radius, color, number });
+      const color = colors[Math.floor(Math.random() * colors.length)];
+      circles.push({ x, y, radius, color });
       i++;
       previousRadius = radius;
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    circles.forEach(({ x, y, radius, color, number }) => {
+    circles.forEach(({ x, y, radius, color }) => {
       // Draw the circle
       ctx.beginPath();
       ctx.arc(x, y, radius, 0, 2 * Math.PI);
@@ -48,12 +49,6 @@ function RandomCircles() {
       ctx.lineWidth = 1;
       ctx.strokeStyle = "#000";
       ctx.stroke();
-
-      // Display radius in circle center
-      ctx.fillStyle = "#fff";
-      ctx.font = "14px sans-serif";
-      ctx.textAlign = "center";
-      ctx.fillText(radius + "px", x, y + 4);
     });
   };
 
@@ -64,7 +59,9 @@ function RandomCircles() {
   return (
     <div>
       <canvas ref={canvasRef} />
-      <button onClick={handleRefreshClick}>Refresh</button>
+      <div style={{ position: "fixed", bottom: 10, left: 10 }}>
+      RandomCircles(), 2023 · <a href="https://felixingla.com"> Felix Ingla</a>
+      </div>
     </div>
   );
 }
